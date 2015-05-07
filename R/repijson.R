535d0acb01c43220b34f0537197a0ccff3a4f753
Thibaut Jombart <thibautjombart@gmail.com>
2015-05-07 14:40:20 +0100

    trying to fix the error in printing ejObjects. Comes from asList_ejAttribute; looks fishy

--- R/repijson.R
+++ R/repijson.R
@@ -1,20 +1,24 @@
-#' A data format for epidemiological data
+#' Classes and functions to handle EpiJSON files
 #'
-#' a JSON format for epidemiological data with functions to convert to and from other useful formats  
+#' EpiJSON is a universal JSON format for epidemiological data. More information on this format can be found at:
+#' http://github.com/Hackout2/EpiJSON
+#'
+#' repijson is a package implementing classes and functions for importing, exporting and converting EpiJSON files.
+#'
 #' @import OutbreakTools jsonlite sp plyr ggplot2
 #' @docType package
 #' @name repijson
 NULL
 
 #' generic as function
-#' 
+#'
 #' @param x an object to convert to repijson
 #' @param ... other parameters to pass to the converter
 #' @export
 as.ejObject <- function(x, ...) UseMethod("as.ejObject")
 
 #' By default we don't know how to convert objects
-#' 
+#'
 #' @param x an object
 #' @param ... other parameters passed to the call
 #' @export
